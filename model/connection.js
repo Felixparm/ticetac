fe5dfaa67b71a8e2e1d769f3c691ce52dd91dc16
felix <fel93@live.fr>
2020-11-05 11:16:26 +0100

    first commit

---
+++ model/connection.js
@@ -0,0 +1,23 @@
+mongoose.connect('mongodb+srv://XXXXXXXX:*********@XXXXXXXX-0hsfc.mongodb.net/Ticketac?retryWrites=true',
+   options,
+   function(err) {
+    if (err) {
+      console.log(`error, failed to connect to the database because --> ${err}`);
+    } else {
+      console.info('*** Database Ticketac connection : Success ***');
+    }
+   }
+);
+
+var journeySchema = mongoose.Schema({
+  departure: String,
+  arrival: String,
+  date: Date,
+  departureTime: String,
+  price: Number,
+});
+
+var journeyModel = mongoose.model('journey', journeySchema);
+
+
+
